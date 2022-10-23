aad28b38546b9e8a6b5fb692eb91ccce1cef075f
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-10-23 23:56:27 +0300

    Add files via upload

---
+++ ilk_ogrenim_kayitlari/php_ders6_switch_case.php
@@ -0,0 +1,48 @@
+<?php
+
+	$gun=7;
+
+	switch ($gun) {
+		case 1:
+			
+			echo "pazartesi";
+		break;
+
+		case 2:
+			
+			echo "sali";
+		break;
+
+		case 3:
+			
+			echo "carsamba";
+		break;
+
+		case 4:
+			
+			echo "persembe";
+		break;
+
+		case 5:
+			
+			echo "cuma";
+		break;
+
+		case 6:
+			
+			echo "cumartesi";
+		break;
+
+		case 7:
+			
+			echo "pazar";
+		break;
+		
+		default:
+			
+			echo "yanlis giris";
+		break;
+	}
+	
+
+?>
