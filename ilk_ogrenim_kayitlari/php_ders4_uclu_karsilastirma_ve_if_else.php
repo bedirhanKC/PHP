2083602e5a867bef057a8ae4b8da924e7e8b799a
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-10-23 23:00:10 +0300

    Add files via upload

---
+++ ilk_ogrenim_kayitlari/php_ders4_uclu_karsilastirma_ve_if_else.php
@@ -0,0 +1,28 @@
+<?php
+
+	$sayi1 = 10;
+	$sayi2 = 30;
+
+	$kontrol = $sayi1 != $sayi2 ? $sayi1." ve ".$sayi2." birbirine esit degildir." : $sayi1." ve ".$sayi2." birbirine esit degildir."; //eger kosul saglaniyorsa ? ile : isaretleri arasi calisir. eger kosul saglanmiyorsa :'dan sonraki kisim calisir.
+
+	echo $kontrol;
+
+	echo "<br><br>";
+
+	if ($sayi1 > $sayi2) {
+		
+		echo "$sayi1 , $sayi2'den buyuktur";
+
+	}else if ($sayi2 > $sayi1 ) {
+		
+		echo "$sayi2, $sayi1'den buyuktur";
+
+	}else{
+
+		echo "$sayi1, $sayi2'ye esittir";
+
+	}
+
+
+
+?>
