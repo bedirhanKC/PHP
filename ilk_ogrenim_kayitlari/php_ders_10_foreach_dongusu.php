1fa23f11e83c851b9b11bc77b36dfa9b342696e5
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-10-27 02:04:30 +0300

    Add files via upload

---
+++ ilk_ogrenim_kayitlari/php_ders_10_foreach_dongusu.php
@@ -0,0 +1,23 @@
+<?php
+
+	$sayilar = range(1, 10);
+
+	foreach ($sayilar as $indeks => $temp) {
+		
+		echo "$indeks. indeksli elemanin degeri : $temp <br>";
+
+	}
+
+	echo "<br><br>";
+
+	$harfler = range("a","z");
+
+	foreach ($harfler as $indeks => $temp) {
+		
+		echo "$indeks. indeksli elemanin degeri : $temp <br>";
+
+	}
+
+
+
+?>
